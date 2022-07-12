3bb5cdd1e165d5fe3ff5dad4f37f8d03dcaf57a7
baltineu <78843628+baltineu@users.noreply.github.com>
2022-07-12 15:33:06 +0300

    19.1 with submit()

--- exercises/19_concurrent_connections/task_19_1.py
+++ exercises/19_concurrent_connections/task_19_1.py
@@ -1,29 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Задание 19.1
-
-Создать функцию ping_ip_addresses, которая проверяет пингуются ли IP-адреса.
-Проверка IP-адресов должна выполняться параллельно в разных потоках.
-
-Параметры функции ping_ip_addresses:
-* ip_list - список IP-адресов
-* limit - максимальное количество параллельных потоков (по умолчанию 3)
-
-Функция должна возвращать кортеж с двумя списками:
-* список доступных IP-адресов
-* список недоступных IP-адресов
-
-Для выполнения задания можно создавать любые дополнительные функции.
-
-Для проверки доступности IP-адреса, используйте ping.
-
-Подсказка о работе с concurrent.futures:
-Если необходимо пинговать несколько IP-адресов в разных потоках,
-надо создать функцию, которая будет пинговать один IP-адрес,
-а затем запустить эту функцию в разных потоках для разных
-IP-адресов с помощью concurrent.futures (это надо сделать в функции ping_ip_addresses).
-"""
-
 from concurrent.futures import *
 from ping3 import ping
 
@@ -37,13 +11,35 @@ def pinger(host):
         return False
 
 
+'''
 def ping_ip_addresses(ip_list, limit=3):
     with ThreadPoolExecutor(limit) as executor:
         result = executor.map(pinger, ip_list)
+        print("Выполнение идет сразу")
         accessible = []
         non_accessible = []
         for ip, output in zip(ip_list, result):
             if output:
+                accessible.append(ip)
+            else:
+                non_accessible.append(ip)
+    return accessible, non_accessible
+'''
+
+
+def ping_ip_addresses(ip_list, limit=3):
+    with ThreadPoolExecutor(limit) as executor:
+
+        future_result_list = []
+
+        for ip in ip_list:
+            future_result = executor.submit(pinger, ip)
+            future_result_list.append(future_result)
+
+        accessible = []
+        non_accessible = []
+        for ip, output in zip(ip_list, future_result_list):
+            if output.result():
                 accessible.append(ip)
             else:
                 non_accessible.append(ip)
